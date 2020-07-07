677671ccc71a3b7382f0124aa6c66db405d5dbf4
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-07-07 10:31:52 +0530

    rendoring components

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ProductList from './ProductList';
+import { Container,Header,Content,H1,Text } from 'native-base';
+
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
 
 export default function App() {
+
+  const user = {
+    firstName:'Greg',
+    lastName:'Lim'
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ProductList />
+    // <Container>
+    //   <Header />
+    //   <Content>
+    //     <H1>Hello,</H1>
+    //     <Text> {formatName(user)} </Text>
+    //   </Content>
+    // </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   textStyle:{
+//     fontSize: 50
+//   }
+// });
