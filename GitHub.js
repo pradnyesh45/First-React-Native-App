0979e0909b1e7beacc43254cfb53722e8f53c1a8
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-08-14 19:30:55 +0530

    search term in axios.get

--- GitHub.js
+++ GitHub.js
@@ -3,12 +3,15 @@ import axios from "axios";
 
 function GitHub() {
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("greg");
   useEffect(() => {
-    axios.get("https://api.github.com/search/users?q=greg").then((res) => {
-      //   console.log(res.data.items);
-      setData(res.data.items);
-    });
-  }, []);
+    axios
+      .get(`https://api.github.com/search/users?q=${searchTerm}`)
+      .then((res) => {
+        console.log(res.data.items);
+        setData(res.data.items);
+      });
+  }, [searchTerm]);
 
   return <div></div>;
 }
